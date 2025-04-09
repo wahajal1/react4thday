8e2a9c58cedeb8b47dd88ed4b8b3a7328817778b
wahajal1 <wahajalosaimi@gmail.com>
2025-04-09 19:58:02 +0300

    change was made on react branch

--- react4t/src/App.jsx
+++ react4t/src/App.jsx
@@ -6,6 +6,7 @@ function app() {
      <h6 style={{backgroundColor: 'red'}}> hi and welcome </h6>
      <span> 7+7 = {7*2}</span>
      <span> change was made</span>
+     <span> change was made on react branch</span>
     </>
   )
 }
