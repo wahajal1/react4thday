24b13be71631f9504319db9caf3cd998bb8e6d25
wahajal1 <wahajalosaimi@gmail.com>
2025-04-09 19:45:52 +0300

    change madec

---
+++ react4t/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+function app() {
+ 
+  return (
+    <>
+     <h6 style={{backgroundColor: 'red'}}> hi and welcome </h6>
+     <span> 7+7 = {7*2}</span>
+     <span> change was made</span>
+    </>
+  )
+}
+
+export default app
