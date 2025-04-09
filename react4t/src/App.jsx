84c7c55cddc31b537a3d03376f9082486cfdb928
wahajal1 <wahajalosaimi@gmail.com>
2025-04-09 20:39:54 +0300

    added h1 component

--- react4t/src/App.jsx
+++ react4t/src/App.jsx
@@ -7,6 +7,7 @@ function app() {
      <span> 7+7 = {7*2}</span>
      <span> change was made</span>
      <span> change was made on react branch</span>
+<h1 style={{color: 'pink'}}> Styling here was made on react branch </h1>
     </>
   )
 }
